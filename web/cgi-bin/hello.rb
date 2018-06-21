0874ec90b35b1d44d917e4429b96eda9c28cf6e2
Kunado <katsusand1@gmail.com>
2018-06-21 18:31:42 +0900

    test executing cgiscript written in ruby

---
+++ web/cgi-bin/hello.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+print "Content-Type: text/html\n\n"
+print "<html>\n"
+print "<head>\n"
+print "<title>CGI Test</title>\n"
+print "</head>\n"
+print "<body>\n"
+print "Hello CGI!\n"
+print "</body>\n"
+print "</html>\n"
