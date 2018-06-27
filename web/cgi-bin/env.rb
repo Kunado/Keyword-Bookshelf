a9f1755d4b5838cf6352c331d93b7d6f5bf2524f
Kunado <katsusand1@gmail.com>
2018-06-28 01:23:30 +0900

    create data files and install ruby-xslt

--- web/cgi-bin/env.rb
+++ web/cgi-bin/env.rb
@@ -1,9 +1,12 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 if ENV['SERVER_NAME'] == 'cgi.u.tsukuba.ac.jp'
-	$CGI_SCRIPTS = ENV['SERVER_NAME'] + '/~s1611442/UT_only/ML'
-	$STATIC_FILES = 'www.u.tsukuba.ac.jp' + '/~s1611442/UT_only/ML'
+	$CGI_SCRIPTS = 'http://' + ENV['SERVER_NAME'] + '/~s1611442/UT_only/ML'
+	$STATIC_FILES = 'http://www.u.tsukuba.ac.jp' + '/~s1611442/UT_only/ML'
 elsif ENV['SERVER_NAME'] == 'localhost'
-	$CGI_SCRIPTS = ENV['SERVER_NAME'] + ':' + ENV['SERVER_PORT'] + '/cgi-bin'
-	$STATIC_FILES = ENV['SERVER_NAME'] + ':' + ENV['SERVER_PORT']
+	$CGI_SCRIPTS = 'http://' + ENV['SERVER_NAME'] + ':' + ENV['SERVER_PORT'] + '/cgi-bin'
+	$STATIC_FILES = 'http://' + ENV['SERVER_NAME'] + ':' + ENV['SERVER_PORT']
 end
+$BOOK_LIST = $STATIC_FILES + '/data/booklist.xml'
+$KEYWORDS = $STATIC_FILES + '/data/keywords.xml'
+$RELATIONS = $STATIC_FILES + '/data/relations.xml'
