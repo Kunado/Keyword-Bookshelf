e87197ae779976317b76944b0ee5de352dd1a606
Kunado <katsusand1@gmail.com>
2018-06-30 04:48:01 +0900

    change page title

--- web/cgi-bin/partial/head.rb
+++ web/cgi-bin/partial/head.rb
@@ -1,7 +1,9 @@
 require_relative '../env'
 $head = <<-"EOS"
 <head>
-    <title>ruby cgi script</title>
+    <title>
+        <%= File.basename(__FILE__) %> - Keyword Bookshelf
+    </title>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="#{$STATIC_FILES}/stylesheets/style.css">
 </head>
