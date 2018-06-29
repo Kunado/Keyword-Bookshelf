738286e1b8df380ff4b170c503218a92667eaf81
Kunado <katsusand1@gmail.com>
2018-06-30 04:44:13 +0900

    add a stylesheet

--- web/cgi-bin/partial/footer.rb
+++ web/cgi-bin/partial/footer.rb
@@ -1,7 +1,9 @@
 $footer = <<-"EOS"
 <footer>
-   <a href="<%= $BOOK_LIST %>">booklist</a>
-   <a href="<%= $KEYWORDS %>">keywords</a>
-   <a href="<%= $RELATIONS %>">relations</a>
+   <div class="container">
+        <a href="<%= $BOOK_LIST %>">booklist</a>
+        <a href="<%= $KEYWORDS %>">keywords</a>
+        <a href="<%= $RELATIONS %>">relations</a>
+    </div>
 </footer>
 EOS
