abfc3d49d84c28673451eb1b54bcf29a38a894be
Kunado <katsusand1@gmail.com>
2018-06-30 18:09:34 +0900

    rewrite README.md

--- web/cgi-bin/partial/footer.rb
+++ web/cgi-bin/partial/footer.rb
@@ -1,9 +1,9 @@
 $footer = <<-"EOS"
 <footer>
    <div class="container">
-        <a href="<%= $BOOK_LIST %>">booklist</a>
-        <a href="<%= $KEYWORDS %>">keywords</a>
-        <a href="<%= $RELATIONS %>">relations</a>
+        <a href="#{$BOOK_LIST}">booklist</a>
+        <a href="#{$KEYWORDS}">keywords</a>
+        <a href="#{$RELATIONS}">relations</a>
     </div>
 </footer>
 EOS
